9d306bf8142087c86ec47454773b2cc7e8f13e41
mcaliskan7 <merve.caliskan@std.yeditepe.edu.tr>
2021-02-10 23:01:10 +0300

    February 10, 2021

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,4 +1,6 @@
 ﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities.Results;
 using DataAccess.Abstract;
 using Entities.Concrete;
 using Entities.DTO;
@@ -17,15 +19,31 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
-        public List<Product> GetAll()
+        public IResult Add(Product product)
         {
-            //Business Code
-            return _productDal.GetAll();
+            //magic strings
+            if (product.ProductName.Length < 2)
+            {
+                return new ErrorResult(Messages.ProductNameInvalid);
+            }
+            _productDal.Add(product);
+            return new SuccessResult(Messages.ProductAdded);
         }
 
-        public List<Product> GetByCategoryID(int id)
+        public IDataResult<List<Product>> GetAll()
+        {
+            //Business Codes
+            return new DataResult(_productDal.GetAll());
+        }
+
+        public List<Product> GetByCategoryId(int id)
         {
             return _productDal.GetAll(p => p.CategoryId == id);
+        }
+
+        public Product GetById(int productId)
+        {
+            return _productDal.Get(p => p.ProductId == productId);
         }
 
         public List<Product> GetByUnitPrice(decimal min, decimal max)
