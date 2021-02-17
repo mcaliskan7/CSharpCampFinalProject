b996cf87bec3bf57df15bdc545a23b2ea587cc39
mcaliskan7 <merve.caliskan@std.yeditepe.edu.tr>
2021-02-18 00:12:00 +0300

    February 17, 2021

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -30,9 +30,11 @@ namespace WebAPI
         public void ConfigureServices(IServiceCollection services)
         {
             //Autofac, Ninject, CastleWindsor, StructureMapp, LightInject, DryInject --> IoC Container
+            //AOP
+            //Postsharp
             services.AddControllers();
-            services.AddSingleton<IProductService, ProductManager>();
-            services.AddSingleton<IProductDal, EfProductDal>();
+            //services.AddSingleton<IProductService, ProductManager>();
+            //services.AddSingleton<IProductDal, EfProductDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
