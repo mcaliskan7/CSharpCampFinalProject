5e87eb475a0ea706f38c86a07116bd59a71b6b79
mcaliskan7 <merve.caliskan@std.yeditepe.edu.tr>
2021-02-25 00:11:12 +0300

    February 24, 2021

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -7,8 +7,12 @@ namespace Business.Constants
     public static class Messages
     {
         public static string ProductAdded = "Product Added.";
+        public static string ProductUpdated = "Product Updated.";
         public static string ProductNameInvalid = "Invalid Product Name!";
         public static string ProductsListed = "Products Listed.";
         public static string MaintenanceTime = "System Maintenance";
+        public static string ProductCountOfCategoryError = "There are more than 15 products in this category.";
+        public static string ProductNameAlreadyExists = "Product name already exists.";
+        public static string CategoryLimitExceeded = "Category Limit Exceeded!";       
     }
 }
